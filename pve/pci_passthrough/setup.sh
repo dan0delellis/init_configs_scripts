f81ff14a08b2756380ace309074affb4e0762178
daniel delellis <dan.delellis@gmail.com>
2024-05-18 03:21:04 -0700

    works i think

--- pve/pci_passthrough/setup.sh
+++ pve/pci_passthrough/setup.sh
@@ -4,7 +4,7 @@ GIT="."
 
 GRUB_DEF=/etc/default/grub
 ETC_MOD=/etc/modules
-MODPROBE=/etc/modprobe.d/
+MODPROBE=/etc/modprobe.d
 
 MP_IUI="$MODPROBE/iommu_unsafe_interrupts.conf"
 MP_KVM="$MODPROBE/kvm.conf"
@@ -28,20 +28,20 @@ fi
 
 
 echo "adding to $GRUB_DEF"
-cat $GIT$GRUB_DEF >> $GRUB_DEF
+sudo bash -c "cat $GIT$GRUB_DEF >> $GRUB_DEF"
 echo "uncomment relevant line, then execute update-grub for changes to take effect"
 
-echo "#options vfio-pci ids=$IDS" >> $MP_VFIO
-echo "#then execute update-initramfs -u" >> $MP_VFIO
+sudo bash -c "echo \"#options vfio-pci ids=$IDS\" >> $MP_VFIO"
+sudo bash -c "echo \"#then execute update-initramfs -u\" >> $MP_VFIO"
 
-for my $i in $MP_IUI $MP_KVM $MP_VFIO; do
-	echo "adding to $i"
-	cat $GIT$i >> $i
+for i in $MP_IUI $MP_KVM $MP_VFIO; do
+	echo "updating $i"
+	sudo bash -c "cat $GIT$i >> $i"
 done
 
 if `$VGA`; then
 	echo "adding to $MP_BL"
-	cat $GIT$MP_BL >> $MP_BL
+	sudo bash -c "cat $GIT$MP_BL >> $MP_BL"
 fi
 
 echo "uncomment relevant lines, then execute 'update-initramfs -u'"
