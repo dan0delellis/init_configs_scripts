6dacd91b1266bc51716494308bf3282d271e513d
Daniel De Lellis <dan.delellis@gmail.com>
2024-11-19 00:33:07 -0800

    add a couple more programs

--- pve/pve_setup.sh
+++ pve/pve_setup.sh
@@ -1,6 +1,6 @@
 cat << EOF > pve_setup.sh
 wget https://raw.githubusercontent.com/foundObjects/pve-nag-buster/635801f8291c801ddf28277b9cf6afd08f0dd2bf/install.sh && md5sum install.sh | grep -q da4ecd060eeaa7e383b6b2583b78ddbb && bash install.sh
 
-apt update; apt upgrade -y; apt install vim screen git wget curl -y
+apt update; apt upgrade -y; apt install vim lm-sensors lshw screen git wget curl -y
 EOF
 bash pve_setup.sh
