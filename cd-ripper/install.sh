abeb5347447cbc64c81a6c9427adf3331461a970
Daniel De Lellis <dan.delellis@gmail.com>
2025-11-06 07:19:46 +0000

    the config is already in git so use that

--- cd-ripper/install.sh
+++ cd-ripper/install.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+../ubuntu/defaults.sh
 sudo apt install -y abcde flac cifs-utils
 sudo mv /etc/abcde.conf{,.default}
-sudo mv abcde.conf /etc/.
-base64 -d <<< "Ly8xMC4xLjAuNS9tZWRpYSAvbW50L21lZGlhIGNpZnMgdmVycz0zLjAsdWlkPTEwMDAsZ2lkPTEwMDAsdXNlcj1zYW1iYXNoYXJlcyxwYXNzPWRvbmtleWJvbmVyLGRlZmF1bHRzLF9uZXRkZXYgMCAwCg==" | sudo tee -a /etc/fstab
+sudo cp abcde.conf /etc/.
+sudo cat ../all/fstab/media >> /etc/fstab
 sudo mkdir /mnt/media
 sudo mkdir /mnt/usb
 sudo mount -a
