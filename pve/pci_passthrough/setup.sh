78ad4293d579501dad5bd659ff1304dc48ca3ef0
Daniel De Lellis <dan.delellis@gmail.com>
2025-11-06 06:14:04 +0000

    don't try to use sudo if sudo isnt present.

--- pve/pci_passthrough/setup.sh
+++ pve/pci_passthrough/setup.sh
@@ -2,6 +2,10 @@
 #
 GIT="."
 
+check_yes() {
+	grep -iq '\(^\s*$\)\|y\(es\)\?' <<< $1 && return 0 || return 1
+}
+
 GRUB_DEF=/etc/default/grub
 ETC_MOD=/etc/modules
 MODPROBE=/etc/modprobe.d
@@ -10,25 +14,26 @@ MP_IUI="$MODPROBE/iommu_unsafe_interrupts.conf"
 MP_KVM="$MODPROBE/kvm.conf"
 MP_BL="$MODPROBE/blacklist.conf"
 MP_VFIO="$MODPROBE/vfio.conf"
-
+SUDO=""
+SUDO=$(which sudo) || SUDO=""
 ADDRESS=
 lspci
 read -p "comma separated addresses of desired device: " ADDRESS
 ADDRESS=$(echo $ADDRESS | sed 's/,/\\|/g')
 VGA=
 IDS=$(lspci -n | grep $ADDRESS | awk {'print $3'} | tr '\n' ','| sed 's/,\s*$//')
-read -p "Are any of these VGA cards? Y/n: " VGA
 
-if $(grep -iq '\(^\s*$\)\|y\(es\)\?' <<< $VGA ); then 
+read -p "Are any of these VGA cards? y/N: " VGA
+
+if $(check_yes "$VGA" ); then 
 	IDS="$IDS disable_vga=1";
 	VGA="true"
 else
 	VGA="false"
 fi
 
-
 echo "adding to $GRUB_DEF"
-sudo bash -c "cat $GIT$GRUB_DEF >> $GRUB_DEF"
+$SUDO bash -c "cat $GIT$GRUB_DEF >> $GRUB_DEF"
 echo "uncomment relevant line, then execute update-grub for changes to take effect"
 
 echo "#options vfio-pci ids=$IDS" >> $GIT$MP_VFIO
@@ -36,12 +41,12 @@ echo "#then execute update-initramfs -u" >> $GIT$MP_VFIO
 
 for i in $MP_IUI $MP_KVM $MP_VFIO; do
 	echo "updating $i"
-	sudo bash -c "cat $GIT$i >> $i"
+	$SUDO bash -c "cat $GIT$i >> $i"
 done
 
 if `$VGA`; then
 	echo "adding to $MP_BL"
-	sudo bash -c "cat $GIT$MP_BL >> $MP_BL"
+	$SUDO bash -c "cat $GIT$MP_BL >> $MP_BL"
 fi
 
 echo "uncomment relevant lines, then execute 'update-initramfs -u'"
