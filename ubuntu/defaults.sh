31ee31d4f199e42070067ddae6d49b10e778f7c0
Daniel De Lellis <dan.delellis@gmail.com>
2025-11-22 00:03:14 -0800

    split functionality into atonmic scripts

--- ubuntu/defaults.sh
+++ ubuntu/defaults.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
+bash apt.sh
+bash git.sh
+bash setup_golang.sh
 sudo apt update; sudo apt remove -y unattended-upgrades needrestart
-sudo apt upgrade -y
-sudo apt install -y vim git screen
-read -p "email for git:" EMAIL
-read -p "name for git:" NAME
-git config --global user.email "$EMAIL"
-git config --global user.name "$NAME"
