015e6cb51fbe623788e8fc25be1ba28877691579
Daniel De Lellis <dan.delellis@gmail.com>
2025-11-22 00:03:25 -0800

    fixes

--- ubuntu/setup_golang.sh
+++ ubuntu/setup_golang.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
-set -x
 TMP=$(mktemp -d)
-ARCH=$(hostnamectl | grep Architecture: | awk {'print $NF'} | sed 's/arm/arm64/;s/x86-/amd/')
+ARCH=$(hostnamectl | grep Architecture: | awk {'print $NF'} | sed 's/arm(64)?/arm64/;s/x86-/amd/')
 
 VER=$(curl -s https://go.dev/VERSION?m=text | grep 'go')
-sudo mv /usr/local/go $TMP
+test -d /usr/local/go && sudo mv /usr/local/go $TMP
 
-curl -s https://dl.google.com/go/$VER.linux-$ARCH.tar.gz | sudo sudo tar -C /usr/local -xz || exit 1
+curl -s https://dl.google.com/go/$VER.linux-$ARCH.tar.gz | sudo tar -C /usr/local -xz || exit 1
 
 sudo rm -rf $TMP;
 grep -q '\/usr\/local\/go\/bin\/' <<< "$PATH" || echo 'export "PATH=$PATH:/usr/local/go/bin"' | sudo tee -a /etc/profile
-
+. /etc/profile
