8bd92657bd230d92b9069b82a02b8b996dc36dc0
Daniel De Lellis <dan.delellis@gmail.com>
2026-08-02 09:43:01 -0700

    jesus this was obnoxiious

--- llm/01-setup.sh
+++ llm/01-setup.sh
@@ -1,48 +1,50 @@
 #!/bin/bash
-set -e
+ONE_API_KEY_URL="https://apt.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS.PUB"
+ONE_API_KEY_PATH="/usr/share/keyrings/oneapi-archive-keyring.gpg"
+#INTEL_REPO_KEY_URL="https://repositories.intel.com/gpu/intel-graphics.key"
+#INTEL_REPO_KEY_PATH="/usr/share/keyrings/intel-graphics.gpg"
+DRIVERS_LIST="/etc/apt/sources.list.d/intel-gpu-drivers.list"
+ONEAPI_LIST="/etc/apt/sources.list.d/one-api.list"
 
-# install things needed to get gpg keys installed
-sudo apt-get update
-sudo apt-get install -y --no-install-recommends ca-certificates curl  gnupg2
+TMPDIR=$(mktemp -d)
 
-#add repos that have nvidia drivers
-source /etc/os-release
-sudo echo "deb http://deb.debian.org/debian/ $VERSION_CODENAME non-free contrib" > /etc/apt/sources.list.d/non-free.list
+LTS="stonking resolute questing plucky noble jammy"
+ERR=
 
-#add nvidia cuda keyring and apt list
-#wget https://developer.download.nvidia.com/compute/cuda/repos/debian13/x86_64/cuda-keyring_1.1-1_all.deb
-#sudo dpkg -i cuda-keyring_1.1-1_all.deb
+check_err() {
+        if [ ! "$ERR" = "" ]; then
+                echo -e $ERR
+		rm -rfv $TMPDIR
+		sudo rm -v $DRIVERS_LIST $ONEAPI_LIST
+                exit 1
+        fi
+}
 
-# install nvidia container package
-curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | sudo gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg
-curl -s -L https://nvidia.github.io/libnvidia-container/stable/deb/nvidia-container-toolkit.list | sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
+sudo true || ERR="superuser privs required"
+check_err
 
-sudo install -m 0755 -d /etc/apt/keyrings
-sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
-sudo chmod a+r /etc/apt/keyrings/docker.asc
-
-# add docker repo
-sudo tee /etc/apt/sources.list.d/docker.sources <<EOF
-Types: deb
-URIs: https://download.docker.com/linux/debian
-Suites: $(. /etc/os-release && echo "$VERSION_CODENAME")
-Components: stable
-Architectures: $(dpkg --print-architecture)
-Signed-By: /etc/apt/keyrings/docker.asc
-EOF
-
-sudo apt update
-sudo apt install -y nvidia-driver firmware-misc-nonfree nvidia-cuda-toolkit nvidia-container-toolkit docker-ce docker-ce-cli containerd.io build-essential cmake git libcurl4-openssl-dev linux-headers-amd64
-
-#wait for networking
-for i in $(seq 1 60); do echo "attempt $i"; host github.com || sleep 1 && break; done
-
-# 4. Clone and compile llama.cpp locally targeting Compute Capability 7.5
-if [ ! -d "llama.cpp" ]; then
-    git -C /root clone https://github.com/ggml-org/llama.cpp.git
-else
-	git -C /root/llama.cpp pull -r
+#TODO move this logic to be activated if $1 and $2 are nonemptyu
+INTEL_GPU_LTS=
+if [ ! "$1" = "" ]; then
+        INTEL_GPU_LTS=$1
 fi
 
-echo "@reboot root $(realpath 02-build.sh)" | sudo tee /etc/cron.d/build-after-boot
-echo "ready 2 reboot"
+lts=$2
+if [ ! $INTEL_GPU_LTS = "" ] && [! $lts = "" ]; then
+	curl $INTEL_REPO_KEY_URL -o "$TMPDIR/apt.pub" #| sudo gpg --dearmor --yes --output $INTEL_REPO_KEY_PATH
+	sudo gpg  --output "$INTEL_REPO_KEY_PATH" --yes --dearmor "$TMPDIR/apt.pub"
+
+	echo "deb [arch=amd64 signed-by=$INTEL_REPO_KEY_PATH] https://repositories.intel.com/gpu/ubuntu ${lts}/lts/${INTEL_GPU_LTS} unified" | sudo tee /etc/apt/sources.list.d/intel-gpu-drivers.list
+fi
+
+sudo apt update
+sudo apt upgrade -y
+sudo apt install -y gnupg wget vim git curl cmake pkg-config build-essential
+
+curl $ONE_API_KEY_URL -o "$TMPDIR/oneapi.pub" #| sudo gpg --dearmor --yes --output $ONE_API_KEY_PATH
+sudo gpg --output "$ONE_API_KEY_PATH" --yes --dearmor "$TMPDIR/oneapi.pub"
+
+echo "deb [signed-by=$ONE_API_KEY_PATH] https://apt.repos.intel.com/oneapi all main" | sudo tee /etc/apt/sources.list.d/one-api.list
+sudo apt update
+
+sudo apt install intel-oneapi-toolkit ocl-icd-libopencl1 intel-opencl-icd libze1 libze-intel-gpu1 libze-intel-gpu-dev -y
